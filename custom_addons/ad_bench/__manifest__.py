0fe4376bc26ca6cb9e9679499bafa0982834c06c
dakhly <a.dakhly89@gmail.com>
2022-07-28 13:35:09 +0300

    [ADD] test

---
+++ custom_addons/ad_bench/__manifest__.py
@@ -0,0 +1,19 @@
+
+
+{
+    'name': 'Bench Financail Rrport',
+    'version': '15.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Ahmed Dakhly',
+    'sequence': '-100',
+    'summary': 'Financail Managment',
+    'description': """
+This module contains all the common features of Financail Reports.
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/ad_bench/models/patient.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+class PatientAccount(models.Model):
+    _name = "patient.account"
+    _description = "Patient Account"
+
+
